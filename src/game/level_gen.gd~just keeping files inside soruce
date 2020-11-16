d25f746b50838351798c8265b11a94f47d5f8aa7
Madhava Prasath <Madhavprasath088@gmail.com>
2020-11-17 01:07:07 +0530

    Auto stash before rebase of "origin/main"

--- src/game/level_gen.gd~just keeping files inside soruce
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var level_blueprint
-var hanging_blueprint
--- src/main/main_menu.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-onready var transiton:Node2D=get_node("Tranisition")
-export(String) var game_scene
-
-
-func _on_TextureButton_pressed():
-	get_tree().change_scene("res://src/game/game.tscn")
