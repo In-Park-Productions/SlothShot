33395c56242a09808cdc310bcc5294e83376a487
Bobby Tagget <bobby.tagget@inparkproductions.com>
2020-11-16 22:35:47 +1000

    Retrieved reference to Sprite Node on Sloth Node

--- SlothShot/src/Scripts/Sloth.gd
+++ SlothShot/src/Scripts/Sloth.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
 var pressed = false
+var sloth: Node2D
 var mousePosition: Vector2
 
 func _ready():
 	set_process_input(true)
+	sloth = get_parent().get_node("Sloth")
+	print(sloth)
 
 func _input(ev):
 	if ev is InputEventMouseButton and ev.button_index == BUTTON_LEFT:
 		pressed = ev.pressed
 		mousePosition = ev.position
-		
+		sloth.position = mousePosition
+		print(str(sloth.position.x) + " " + str(mousePosition.x))
 
-func _physics_process(delta):
-	print(mousePosition.x)
+# func _physics_process(delta):	
