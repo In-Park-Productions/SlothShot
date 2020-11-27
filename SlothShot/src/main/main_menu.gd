004ef290a9b569304e4adb049b598104d6aee2c5
Timothy Chew <wentimothychewyu@gmail.com>
2020-11-28 00:44:41 +1000

    Added Main Menu

--- SlothShot/src/main/main_menu.gd
+++ SlothShot/src/main/main_menu.gd
@@ -2,6 +2,15 @@ extends Node2D
 onready var transiton:Node2D=get_node("Tranisition")
 export(String) var game_scene
 
-
-func _on_TextureButton_pressed():
+func StartButton_Pressed():
 	get_tree().change_scene("res://src/game/game.tscn")
+
+func QuitButton_Pressed():
+	get_tree().quit()
+
+func TestSceneButton_Pressed():
+	get_tree().change_scene("res://src/tests/Test.tscn")
+
+
+func TestButton_Pressed():
+	pass # Replace with function body.
