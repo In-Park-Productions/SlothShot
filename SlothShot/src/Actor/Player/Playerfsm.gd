f6aa3f6e03bfe64c208a1e7352afc4c5530b68e5
Madhava Prasath <Madhavprasath088@gmail.com>
2020-12-27 12:16:42 +0530

    Physic

--- SlothShot/src/Actor/Player/Playerfsm.gd
+++ SlothShot/src/Actor/Player/Playerfsm.gd
@@ -4,26 +4,29 @@ extends "res://src/Statemachine/mainFsm.gd"
 #state stuff
 
 class drag:
+	#tween under constant so it will help us to understand
 	const tween_sine=Tween.TRANS_SINE
 	const EASE_IN_OUT=Tween.EASE_IN_OUT
+	#makes 
 	var dragging:bool=false
 	var anim:String="Short"
 	var current_mouse_position:Vector2=Vector2() 
 	var last_mouse_position:Vector2=Vector2()
 	var diffrence:float=0.0
-	enum{short,long}
-	var current_mode=short
 	enum {short_check,long_check}
 	var check
 	var backward
-
+class launched:
+	var fire_velocity:Vector2=Vector2()
+	var mouse_position:Vector2
+	var time_of_flight:float
+	var final_velocity:Vector2=Vector2()
+	enum{launched,moving,none}
+	var path=none
 func _init():
 	states={
-		1:"Holding_branch",
-		"Launched":{
-			1:"Assend",
-			2:"decend"
-		},
+		1:"Idle",
+		2:"Launched",
 		3:"Dragged",
 		4:"Fight",
 		5:"Dead"
@@ -32,26 +35,38 @@ func _init():
 
 
 onready var drag_state=drag.new()
+onready var launch_state=launched.new()
 
-
-func _ready():
+func _ready()->void:
 	current_state=states[1]
+	randomize()
 	pass
 
-func state_logic(delta):
-	if drag_state.dragging:
+func state_logic(delta)->void:
+	if current_state in ["Dragged"]:
 		on_mousebutton_pressed()
-	if !drag_state.dragging:
-		on_mousebutton_released()
+	if current_state in ["Launched"]:
+			on_mousebutton_released()
+			if launch_state.path==launch_state.launched:
+				calculate_the_trajectory()
 	check_dragging_released()
 
 func transition(delta):
+	match current_state:
+		"Idle":
+			if drag_state.dragging:
+				return states[3]
+		"Dragged":
+			if !drag_state.dragging:
+				return states[2]
+		"Assend":
+				return null
+			
 	return null
 
 func _enter_state(_old_state,_new_state):
-	._enter_state(_old_state,_new_state)
 	match _new_state:
-		"_":
+		'_':
 			pass
 
 
@@ -64,25 +79,29 @@ func _exit_state(_new_state,_old_state):
 #this is for dragstate 
 func _on_Drag_Area_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton && event.button_index==BUTTON_LEFT:
+		#it triggers the drag_state
 		drag_state.dragging=true
 
-func check_dragging_released():
+
+func check_dragging_released()->void:
 	if Input.is_action_just_released("Click"):
+		#it makes the 
 		drag_state.dragging=false
 
-func on_mousebutton_pressed():
-	parent.previous_mouse_position=Vector2()
-	var diffrence_mouse_poisiton:Vector2=parent.global_position-parent.get_global_mouse_position()
-	parent.drag_vector=diffrence_mouse_poisiton
-	parent.drag_vector=parent.drag_vector.clamped(100)
+func on_mousebutton_pressed()->void:
+	var diffrence_mouse_position:Vector2=parent.global_position-parent.get_global_mouse_position()
+	parent.drag_vector=parent.get_global_mouse_position()-parent.global_position
+	parent.drag_vector=parent.drag_vector.clamped(80)
 	rotate_sloth()
-	Animatate_sloth_acording_to_mouse_position(diffrence_mouse_poisiton)
+	Animatate_sloth_acording_to_mouse_position(diffrence_mouse_position)
 
-func on_mousebutton_released():
-	parent.previous_mouse_position=parent.get_local_mouse_position()
-	parent.previous_mouse_position=parent.previous_mouse_position.clamped(100)
+func on_mousebutton_released()->void:
+	if launch_state.path==launch_state.none:
+		launch_state.mouse_position =parent.global_position-parent.get_global_mouse_position()
+		launch_state.path=launch_state.launched
+	
 
-func rotate_sloth():
+func rotate_sloth()->void:
 	var temp=parent.global_position-parent.get_global_mouse_position()
 	var rotation=(temp).angle() if temp.x>0 else parent.rotation
 	var tween = Tween.new()
@@ -91,12 +110,11 @@ func rotate_sloth():
 	tween.start()
 
 func Todo():
-	#TODO:Complete player animation
+	#TODO:Complete player physics
 	pass
 
 func Animatate_sloth_acording_to_mouse_position(mouse_position:Vector2)->void:
-	drag_state.diffrence=calculate_facing(parent.get_local_mouse_position())
-	
+	calculate_facing(parent.get_local_mouse_position())
 	if drag_state.diffrence==1:
 		drag_state.backward=false
 	elif drag_state.diffrence==-1:
@@ -129,32 +147,34 @@ func Animatate_sloth_acording_to_mouse_position(mouse_position:Vector2)->void:
 			pass
 
 
-func calculate_facing(mouse_position:Vector2)->float:
+func calculate_facing(mouse_position:Vector2)->void:
 	drag_state.current_mouse_position=mouse_position
 	var diffrence=int(drag_state.current_mouse_position.x-drag_state.last_mouse_position.x)
+	yield(get_tree(),"physics_frame")
 	drag_state.last_mouse_position=drag_state.current_mouse_position
-	return sign(-diffrence)
-
+	drag_state.diffrence=sign(-diffrence)
 
 #calls in animation_player(Drag_state)
-func on_short_finished():
-	if drag_state.diffrence==1:
-		drag_state.anim="Long"
-	else:
+func on_short_finished()->void:
+	if drag_state.diffrence==-1:
 		drag_state.anim="Short"
+	else:
+		drag_state.anim="Long"
 
-func on_long_finished():
+func on_long_finished()->void:
 	if drag_state.anim=="Long"&&drag_state.diffrence==1:
 		drag_state.check=drag_state.long_check
 
-func on_long_start():
+func on_long_start()->void:
 	if drag_state.diffrence==-1:
-		drag_state.backward=true
 		drag_state.anim="Short"
-	else:
+	elif drag_state.diffrence==1:
 		drag_state.anim="Long"
-		drag_state.backward=false
 
-func on_short_start():
+func on_short_start()->void:
 	if drag_state.anim=="Short"&&(drag_state.diffrence==-1 || drag_state.diffrence==0):
-		drag_state.check=drag_state.short
+		drag_state.check=drag_state.short_check
+#Launched state
+func calculate_the_trajectory():
+	if launch_state.path==launch_state.launched:
+		parent. apply_velocity(launch_state.mouse_position,parent.LaunchVelocity)
--- SlothShot/src/Actor/Player/Playerscripts/Player.gd
+++ SlothShot/src/Actor/Player/Playerscripts/Player.gd
@@ -1,13 +1,15 @@
 extends KinematicBody2D
 
 
-const speed:float=800.0
-const Gravity:float=750.0
+const speed:float=400.0
+const Gravity:float=500.0
 const Weight:float=0.4
 
 var previous_mouse_position:Vector2=Vector2()
 var drag_vector:Vector2=Vector2() 
-var LaunchVelocity:Vector2=Vector2() setget set_lauch_velocity
+var LaunchVelocity:Vector2=Vector2() 
+enum{assend,decend}
+var mode=assend
 
 onready var body:Node2D=get_node("Body")
 onready var animation_player:AnimationPlayer=get_node("Body/AnimationPlayer")
@@ -15,8 +17,8 @@ onready var raycasts:Node2D=get_node("Body/Raycast")
 onready var tween:Tween=get_node("Body/Tween")
 onready var animated_sprite:AnimatedSprite=get_node("Body/AnimatedSprite")
 
-func _on_Player_time_flight_instanciated(time_of_flight):
-	yield(get_tree().create_timer(time_of_flight),"timeout")
+
+
 
 func check_for_collision():
 	for raycast in raycasts:
@@ -24,16 +26,41 @@ func check_for_collision():
 			return true
 	return false
 
-func Apply_velocity():
-	LaunchVelocity=move_and_slide(LaunchVelocity,Vector2.UP)
+
 
 func Fight():
 	pass
 func Swim():
 	pass
-func set_lauch_velocity(velocity):
+
+
+func when_launched()->void:
 	pass
 
 
+func calculate_trajectory(Mouse_position:Vector2,air_resistance:float=0.0,facing:float=1.0)->Array:
+	var mouse_length=Mouse_position.length()
+	mouse_length=clamp(mouse_length,0.0,600)
+	var normalized_mouse_position=Mouse_position.normalized()
+	var length=mouse_length/1000 if Mouse_position.y>0 else -(mouse_length/1000)
+	var angle=Mouse_position.angle()
+	var componets={'x':cos(angle),
+					'y':sin(angle)+Gravity}
+	var time_of_flight=(speed*sin(angle)/float(Gravity+air_resistance)+1)
+	var final_speed=[]
+	final_speed.append(Vector2(componets['x']*speed*facing*normalized_mouse_position.x,componets['y']*length))
+	return [final_speed,time_of_flight]
 
 
+func apply_velocity(mouse_position:Vector2,Velocity:Vector2)->void:
+	var return_array=calculate_trajectory(mouse_position,0.0,sign(mouse_position.x))
+	var velocity=return_array[0][0] if return_array[0][0].x>0 else Vector2()
+	var time_of_flight=return_array[1]
+	if mode==assend:
+		LaunchVelocity=velocity
+		LaunchVelocity=move_and_slide(LaunchVelocity)
+		yield(get_tree().create_timer(time_of_flight),"timeout")
+		mode=decend
+	elif mode==decend:
+		LaunchVelocity.y+=Gravity*get_physics_process_delta_time()
+		LaunchVelocity=move_and_slide(LaunchVelocity,Vector2.UP)
