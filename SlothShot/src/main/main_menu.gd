15d7e690022b8648b7a8f4c08ddd73d73f11611f
Bobby Tagget <bobby.tagget@inparkproductions.com>
2020-12-31 00:34:31 +1000

    Connected man menu start to game world

--- SlothShot/src/main/main_menu.gd
+++ SlothShot/src/main/main_menu.gd
@@ -5,7 +5,7 @@ export(String) var game_scene
 onready var GlobalStream = get_node("/root/GlobalStream")
 
 func StartButton_Pressed():
-	get_tree().change_scene("res://src/game/game.tscn")
+	get_tree().change_scene("res://src/game/game_world.tscn")
 
 func QuitButton_Pressed():
 	get_tree().quit()
