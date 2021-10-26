d1f2f52e9212a279edfe02f47c5ac995852c6db2
Madhavprasath <Madhavprasath088@gmail.com>
2021-10-26 16:15:49 +0530

    Logger Bug fixes

--- src/Objects/Trees.gd
+++ src/Objects/Trees.gd
@@ -5,4 +5,10 @@ onready var animation_player=get_node("AnimationPlayer")
 
 func _on_Trees_area_entered(area):
 	if area.is_in_group("Cut"):
-		animation_player.play("Fall")
+		var logger=area.get_parent().get_parent()
+		if logger.search_direction==-1:
+			$Tween.interpolate_property(self,"rotation_degrees",90,0,1)
+		elif logger.search_direction==1:
+			$Tween.interpolate_property(self,"rotation_degrees",90,180,1)
+		$Tween.start()
+		$CollisionShape2D.set_deferred("disabled",true)
